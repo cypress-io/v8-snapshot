acb2b5a40fb5e3cc1c49cfb2c4e569bd06be4b9b
Thorsten Lorenz <thlorenz@gmx.de>
2020-11-10 15:44:31 -0500

    example: updating examples to new API

--- example-minimal/snapshot/install-snapshot.js
+++ example-minimal/snapshot/install-snapshot.js
@@ -7,10 +7,19 @@ const snapshotEntryFile = process.env.RENDERER
   ? require.resolve('../app/renderer')
   : require.resolve('./snapshot.js')
 
+if (process.env.BUNDLER == null) {
+  console.error(
+    'Need to provide path to bundler via "BUNDLER=<bundler> node install-snaphot"'
+  )
+  process.exit(1)
+}
+
+const bundlerPath = path.resolve(process.env.BUNDLER)
 const snapshotGenerator = new SnapshotGenerator(
+  bundlerPath,
   projectBaseDir,
   snapshotEntryFile,
-  { minify: false }
+  { minify: false, verify: true }
 )
 
 ;(async () => {
--- example/snapshot/install-snapshot.js
+++ example/snapshot/install-snapshot.js
@@ -2,12 +2,21 @@ process.env.DEBUG = 'snapgen:*'
 const path = require('path')
 const { SnapshotGenerator } = require('../../')
 
-const projectBaseDir = path.join(__dirname, '../')
+const projectBaseDir = path.resolve(__dirname, '..')
 const snapshotEntryFile = process.env.RENDERER
   ? require.resolve('../app/renderer')
   : require.resolve('./snapshot.js')
 
+if (process.env.BUNDLER == null) {
+  console.error(
+    'Need to provide path to bundler via "BUNDLER=<bundler> node install-snaphot"'
+  )
+  process.exit(1)
+}
+
+const bundlerPath = path.resolve(process.env.BUNDLER)
 const snapshotGenerator = new SnapshotGenerator(
+  bundlerPath,
   projectBaseDir,
   snapshotEntryFile
 )
