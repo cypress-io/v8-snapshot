cce366c20a1dd4ed614bb6f726bb4efb8c0dccfe
Thorsten Lorenz <thlorenz@gmx.de>
2021-08-25 10:45:59 -0500

    windows: test to expose backslash in key issue

---
+++ src/tests/esbuild/windows-issues.test.ts
@@ -0,0 +1,46 @@
+import path from 'path'
+import test from 'tape'
+import { SnapshotGenerator } from '../../snapshot-generator'
+import { exec as execOrig } from 'child_process'
+import { promisify } from 'util'
+
+const exec = promisify(execOrig)
+
+const projectBaseDir = path.join(__dirname, 'fixtures', 'windows-issues')
+const cacheDir = path.join(projectBaseDir, 'cache')
+const snapshotEntryFile = path.join(projectBaseDir, 'entry.js')
+
+test('esbuild: windows-issues', async (t) => {
+  const generator = new SnapshotGenerator(projectBaseDir, snapshotEntryFile, {
+    cacheDir,
+    nodeModulesOnly: false,
+  })
+  await generator.createScript()
+  await generator.makeAndInstallSnapshot()
+
+  const env: Record<string, any> = {
+    ELECTRON_RUN_AS_NODE: 1,
+    DEBUG: '(packherd|snapgen):*',
+    PROJECT_BASE_DIR: projectBaseDir,
+    DEBUG_COLORS: 1,
+  }
+  const cmd =
+    `node ${require.resolve('electron/cli')}` +
+    ` -r ${projectBaseDir}/hook-require.js` +
+    ` ${projectBaseDir}/app.js`
+
+  let stdout, stderr
+  try {
+    ;({ stdout, stderr } = await exec(cmd, { env }))
+    const res = JSON.parse(stdout.trim())
+    t.equal(
+      res.unsupported,
+      'babel-unsupported',
+      'loads ./babel/unsupportedIterableToArray.js'
+    )
+  } catch (err) {
+    console.log(stdout)
+    console.log(stderr)
+    t.fail(err.toString())
+  }
+})
