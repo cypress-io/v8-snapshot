001fadc1673012bf5dd137adade8cb4379705825
Thorsten Lorenz <thlorenz@gmx.de>
2021-10-21 15:58:20 -0500

    chore: documenting types

--- src/types.ts
+++ src/types.ts
@@ -7,6 +7,15 @@ export type Entries<T> = {
   [K in keyof T]: [K, T[K]]
 }[keyof T][]
 
+/**
+ * esbuild metadata {@link https://esbuild.github.io/api/#metafile} with extra
+ * properties that is included by the snapshot esbuild
+ *
+ * Namely it includes a `resolverMap` property which is embedded into the
+ * snapshot in order to resolve modules without having to query the file system
+ *
+ * @category snapshot
+ */
 export type Metadata = CreateBundleResult['metafile'] & {
   inputs: Record<
     string,
@@ -24,6 +33,46 @@ export type Metadata = CreateBundleResult['metafile'] & {
   resolverMap: Record<string, string>
 }
 
+/**
+ * Configures how the bundle to be snapshotted is generated.
+ *
+ * @property baseDirPath root of the project which we are snapshotting
+ *
+ * @property entryFilePath file we use as the entry-point and from which all
+ * modules to be snapshotted are reachable
+ *
+ * @property bundlerPath the esbuild bundler binary to use, if not provided it
+ * falls back to the installed one
+ *
+ * @property nodeModulesOnly if `true` only node_modules are included in the
+ * snapshot, i.e. application files are not
+ *
+ * @property deferred you should provide any modules here that you know need
+ * to be deferred in order to speed up the doctor step
+ *
+ * @property norewrite you should provide any modules here that you know
+ * should not be rewritten in order to speed up the doctor step and to work
+ * around issues due to invalid rewrites
+ *
+ * @property includeStrictVerifiers if `true` the bundle will be more strictly
+ * checked when validated inside the Node.js VM
+ * This should be set when running the doctor and unset when building the
+ * bundle to be snapshotted
+ *
+ * @property sourcemap if `true` then a sourcemap will be generated for the
+ * bundled files
+ *
+ * @property sourcemapExternalPath the file to write the generated sourcemap
+ * to if that is desired
+ *
+ * @property sourcemapEmbed when `true` the sourcemap is embedded in the
+ * snapshot
+ *
+ * @property sourcemapInline when `true` the sourcemap is inlined at the
+ * bottom of the bundled file that is snapshotted
+ *
+ * @category snapshot
+ */
 export type CreateBundleOpts = {
   baseDirPath: string
   entryFilePath: string
@@ -38,12 +87,30 @@ export type CreateBundleOpts = {
   sourcemapInline: boolean
 }
 
+/**
+ * Adds Snapshot specific opts to the {@link CreateBundleOpts}.
+ *
+ * @property resolverMap the map that should be embedded in the snapshot in
+ * order to resolve module import requests without querying the file system
+ *
+ * @property auxiliaryData any extra data that should be embedded in the
+ * snapshot
+ *
+ * @property nodeEnv `process.env.NODE_ENV` will be set to this value during
+ * snapshot creation, see src/blueprint.ts:88
+ *
+ * @category snapshot
+ */
 export type CreateSnapshotScriptOpts = CreateBundleOpts & {
   resolverMap?: Record<string, string>
   auxiliaryData?: Record<string, any>
   nodeEnv: string
 }
 
+/**
+ * Used to configure the workers that are processing a snapshot script in parallel
+ * @category snapshot
+ */
 export type ProcessScriptOpts = {
   bundleHash: string
   bundlePath: string
@@ -55,30 +122,66 @@ export type ProcessScriptOpts = {
   nodeEnv: string
 }
 
+/**
+ * Possible outcomes of processing a snapshot script.
+ *
+ * - 'failed:assembleScript' means that the bundler generated a proper bundle
+ * but it couldn't be included in the snapshot script
+ * - 'failed:verifyScript' means that the script was assembled fine, but some
+ * violation was detected during the verification phase which indicates that it
+ * couldn't be snapshotted as is
+ * - 'completed' all went fine the script could be snapshotted as is
+ *
+ * @category snapshot
+ */
 export type ProcessScriptResult = {
   outcome: 'failed:assembleScript' | 'failed:verifyScript' | 'completed'
   error?: Error
 }
+/** Adds an outcome to the possible {@link ProcessScriptResult} outcomes.
+ *
+ * - 'failed:bundleScript' means that the bundler failed to generate a proper
+ * bundle, i.e. compilation failed most likely due to an invalid rewrite
+ *
+ * @category snapshot
+ */
 export type BundleAndProcessScriptResult = {
   outcome: 'failed:bundleScript' | ProcessScriptResult['outcome']
   error?: Error
 }
 
+/** Specifies the signature of the function that represents a module definition
+ * and when invoked returns a Node.js `module`.
+ *
+ * Note that an `exports` parameter which is the same instance as the
+ * `module.exports` field.  The module either mutates the `exports` directly or
+ * reassigns `module.exports`.  After invoking it, the `module.exports` are
+ * considered the exports of the module.
+ *
+ * These definitions are embedded into the snapshot and invoked at runtime.
+ * They are used instead of full-fledged exports when snapshotting would fail
+ * were we to initialize them during the snapshot phase.
+ *
+ * @category snapshot
+ * @category loader
+ */
 export type ModuleDefinition = (
   exports: NodeModule['exports'],
-  module: { exports: NodeModule['exports'] },
+  module: {
+    exports: NodeModule['exports']
+  },
   __filename: string,
   __dirname: string,
   require: NodeRequireFunction
 ) => NodeModule
 
-export type SnapshotResult = {
-  customRequire: {
-    cache: Record<string, NodeModule>
-    definitions: Record<string, ModuleDefinition>
-  }
-}
-
+/**
+ * The result of snapshotting a snapshot script. Namely it has the
+ * `customRequire` function which also references the `exports` containing fully
+ * initialized modules as well as `definitions` {@link ModuleDefinition}..
+ *
+ * @category snapshot
+ */
 export type Snapshot = {
   customRequire: {
     definitions: Record<string, NodeRequireFunction>
@@ -88,6 +191,10 @@ export type Snapshot = {
   }
 }
 
+/**
+ * Extra data we include in the snapshot, namely the embedded `sourceMap`.
+ * @category snapshot
+ */
 export type SnapshotAuxiliaryData = {
   sourceMap?: RawSourceMap
 }
